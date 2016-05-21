fcb41d7c953dfb17543f0b07701b71b37f84a3d2
Vlad Kuzmin <tapatun2005@gmail.com>
2016-05-21 12:33:14 +0100

    cards for 4 games

--- gulpfile.js
+++ gulpfile.js
@@ -72,22 +72,35 @@ gulp.task('json', function() {
     //   src: 'json/888poker - game_4.json',
     //   namespace: 'game_4'
     // }))
-    // .pipe(json({
-    //   src: 'json/888poker - chips_4.json',
-    //   namespace: 'chips_4'
+    //  .pipe(json({
+    //     src: 'json/888poker - game_5.json',
+    //     namespace: 'game_5'
     // }))
     // .pipe(json({
     //   src: 'json/888poker - chips_1.json',
     //   namespace: 'chips_1'
     // }))
-    .pipe(json({
-      src: 'json/888poker - chips_2.json',
-      namespace: 'chips_2'
-    }))
+    // .pipe(json({
+    //   src: 'json/888poker - chips_2.json',
+    //   namespace: 'chips_2'
+    // }))
     //   .pipe(json({
     //     src: 'json/888poker - chips_3.json',
     //     namespace: 'chips_3'
     // }))
+    // .pipe(json({
+    //   src: 'json/888poker - chips_4.json',
+    //   namespace: 'chips_4'
+    // }))
+    //   .pipe(json({
+    //     src: 'json/888poker - chips_5.json',
+    //     namespace: 'chips_5'
+    // }))
+       .pipe(json({
+        src: 'json/888pokerdata - tableCards.json',
+        namespace: 'tableCards'
+    }))
+    
   .pipe(concat('data.js'))
   .pipe(gulp.dest('src/js'));
 });
