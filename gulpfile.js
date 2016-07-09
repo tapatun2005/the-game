75c6f0228845943e09761bd5db3a4ba764be7055
Vlad Kuzmin <tapatun2005@gmail.com>
2016-07-09 16:53:34 +0100

    4 games chips

--- gulpfile.js
+++ gulpfile.js
@@ -89,18 +89,18 @@ gulp.task('json', function() {
     //     src: 'json/888poker - chips_3.json',
     //     namespace: 'chips_3'
     // }))
-    // .pipe(json({
-    //   src: 'json/888poker - chips_4.json',
-    //   namespace: 'chips_4'
-    // }))
+    .pipe(json({
+      src: 'json/888poker - chips_4.json',
+      namespace: 'chips_4'
+    }))
     //   .pipe(json({
     //     src: 'json/888poker - chips_5.json',
     //     namespace: 'chips_5'
     // }))
-       .pipe(json({
-        src: 'json/888pokerdata - tableCards.json',
-        namespace: 'tableCards'
-    }))
+    //    .pipe(json({
+    //     src: 'json/888pokerdata - tableCards.json',
+    //     namespace: 'tableCards'
+    // }))
     
   .pipe(concat('data.js'))
   .pipe(gulp.dest('src/js'));
