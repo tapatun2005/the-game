463c430ece4cbec3ff18657a6c8e1a5ed7d39dc7
Dasha Zhurauskaya <malfille@Dashas-iMac.local>
2016-09-06 00:10:55 +0100

    latest

--- src/js/base.js
+++ src/js/base.js
@@ -2012,7 +2012,7 @@ require("hammerjs");
     var social = {
           "texas-holdem": {
             "title": "Texas Holdem - an Interactive Poker Guide by 888poker",
-            "description_facebook": "Learn how to play the most popular card game in the world in no time with The Game visual guide. Don't waste time, learn Texas Holdem and start your first hand right now.",
+            "description_facebook": "Learn how to play the most popular card game with The Game visual guide. Don't waste time, learn Texas Holdem and start your first hand right now.",
             "description_twitter":"Learn how to play Texas Holdem in no time with our @888poker visual guide. Don't waste time! Start your first hand right now. #thegame",
             "picture": "http://game888-2.s3-website-eu-west-1.amazonaws.com/images/share-texas-holdem.jpg",
             "caption": "888poker",
@@ -2020,7 +2020,7 @@ require("hammerjs");
           },
           "omaha": {
             "title": "Omaha Hi - an Interactive Poker Guide by 888poker",
-            "description_facebook": "The Omaha Hi (PLO) visual guide is a perfect place to start learning this exciting poker game brought to you by 888poker. Get the inside scoop on how to play Omaha in a few simple steps.",
+            "description_facebook": "The Omaha Hi (PLO) visual guide is a perfect place to start learning this exciting game. Get the inside scoop on how to play Omaha in a few simple steps.",
             "description_twitter":"The Omaha Hi (PLO) visual guide is a perfect place to start learning this exciting poker game brought to you by @888poker #thegame",
             "picture": "http://game888-2.s3-website-eu-west-1.amazonaws.com/images/share-omaha.jpg",
             "caption": "888poker",
@@ -2028,7 +2028,7 @@ require("hammerjs");
           },
           "omaha-hi-lo": {
             "title": "Omaha Hi-Lo - an Interactive Poker Guide by 888poker",
-            "description_facebook": "Omaha Hi-Lo is one of the most popular poker games with two winners. Learn how to play this exciting game with our 888poker visual guide and get more chances to win!",
+            "description_facebook": "Omaha Hi-Lo is one of the most popular poker games with two winners. Learn how to play this exciting game with our visual guide. Get more chances to win!",
             "description_twitter":"Omaha Hi-Lo is a poker games with two winners. Learn how to play this exciting game with our @888poker visual guid #thegame",
             "picture": "http://game888-2.s3-website-eu-west-1.amazonaws.com/images/share-omaha-hi-lo.jpg",
             "caption": "888poker",
@@ -2037,7 +2037,7 @@ require("hammerjs");
           "seven-card-stud": {
             "title": "7 Card Stud - an Interactive Poker Guide by 888poker",
             "description_twitter":"Seven Card Stud is a great alretnative to Texas Holdem poker. Learn to play the \"father of poker\" with @888poker visual guide. #thegame",
-            "description_facebook": "Seven Card Stud is a great alretnative to Texas Holdem poker. Learn to play the \"father of poker\" in no time with The Game visual guide.",
+            "description_facebook": "Seven Card Stud is agreat alretnative to Texas Holdem poker. Learn to play the &#34;father of poker&#34; in no time with The Game visual guide.",
             "picture": "http://game888-2.s3-website-eu-west-1.amazonaws.com/images/share-seven-card-stud.jpg",
             "caption": "888poker",
             "link": "www.888poker.com"
@@ -2045,7 +2045,7 @@ require("hammerjs");
           "snap": {
             "title": "SNAP poker - an Interactive Poker Guide by 888poker",
             "description_twitter":"SNAP poker is a fast-fold game format designed to reduce the time between a fold and a new hand. Learn SNAP poker today! #thegame @888poker",
-            "description_facebook": "SNAP poker is a fast-fold game format designed to reduce the time between a fold and a new hand. Learn SNAP poker today and won't have to wait long to win!",
+            "description_facebook": "SNAP poker is a fast-fold game format designed to reduce the time between a fold and a new hand. Learn SNAP today and won't have to wait long to win!",
             "picture": "http://game888-2.s3-website-eu-west-1.amazonaws.com/images/share-snap.jpg",
             "caption": "888poker",
             "link": "www.888poker.com"
